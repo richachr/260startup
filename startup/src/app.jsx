feec371abe8a1fe5114d78a84fe1332ccc774f76
richachr <techzgamingdevs@gmail.com>
2025-02-12 15:21:08 -0700

    Added page not found functionality.

--- startup/src/app.jsx
+++ startup/src/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './app.css';
-import {Routes, Route, NavLink} from 'react-router-dom';
+import {Routes, Route, NavLink, Navigate} from 'react-router-dom';
 
 export default function App() {
     return (
@@ -11,6 +11,7 @@ export default function App() {
                         <Route index element={<Welcome />} />
                         <Route path='/login' element={<Login />} />
                         <Route path='/create-account' element={<CreateAccount />} />
+                        <Route path='/not-found' element={<NotFound />} />
                     </Route>
                 </Route>
                 <Route element={<PostAuthHeader />}>
@@ -18,11 +19,17 @@ export default function App() {
                     <Route path='/create-appointment' element={<CreateAppointment />} />
                     <Route path='/scheduler' element={<Scheduler />} />
                 </Route>
+                <Route path='*' element={<ToNotFound />} />
             </Routes>
             <footer>
                 <h4>Created by Christian Richardson</h4>
-                <a href="https://github.com/richachr/260startup"><i class="fa-brands fa-github"></i><span>GitHub</span></a>
+                <a href="https://github.com/richachr/260startup"><i className="fa-brands fa-github"></i><span>GitHub</span></a>
             </footer>
         </div>
     )
+}
+
+function ToNotFound() {
+    const navigate = useNavigate();
+    navigate('/not-found')
 }
