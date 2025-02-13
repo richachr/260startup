76b18f1d810ca87c730696468d0a87eea9769a66
richachr <techzgamingdevs@gmail.com>
2025-02-13 13:22:59 -0700

    Changed HTML to fit JSX syntax.

--- startup/src/postAuth/scheduler/scheduler.jsx
+++ startup/src/postAuth/scheduler/scheduler.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 export function Scheduler() {
+    const navigate = useNavigate();
     return (
         <div class="mainContent">
-            <img src="papers.png" alt="Person doing paperwork">
+            <img src="papers.png" alt="Person doing paperwork" />
             <h1>Let's talk time.</h1>
             <h3>How do these appointment times sound?</h3>
             <fieldset class="fieldset">
@@ -18,14 +21,13 @@ export function Scheduler() {
                     <label for="3"><input type="radio" name="apptTime" id="3" value="3" />Monday, February 10th 3:30pm</label>
                 </div>
             </fieldset>
-                <div class="actions">
-                    <button type="button" class="secondary"><span>Show more times for this day</span></button>
-                    <button type="button" class="secondary"><span>Show the next available day</span></button>
-                </div>
-            </form>
+            <div class="actions">
+                <button type="button" class="secondary"><span>Show more times for this day</span></button>
+                <button type="button" class="secondary"><span>Show the next available day</span></button>
+            </div>
             <nav>
-                <a href="create.html"><button type="button" class="danger"><span>Back</span></button></a>
-                <a href="appointments.html"><button type="button" class="primary"><i class="fa-solid fa-arrow-right"></i></button></a>
+                <button onClick={()=>navigate(-1)} type="button" class="danger"><span>Back</span></button>
+                <NavLink to="appointments"><button type="button" class="primary"><FontAwesomeIcon icon={faArrowRight} className="fontAwesome" /></button></NavLink>
             </nav>
         </div>
     )
