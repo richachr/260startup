0b4537d2e769e22da03863f5681cac560df0760b
richachr <techzgamingdevs@gmail.com>
2025-02-12 18:39:58 -0700

    Added Homepage function (left content on welcome).

--- startup/src/preAuth/welcome/welcome.jsx
+++ startup/src/preAuth/welcome/welcome.jsx
@@ -3,6 +3,11 @@ import {NavLink} from 'react-router-dom';
 
 export function Homepage() {
     return (
-        
+        <div className="mainLeftContent">
+                <img src="doc-patient.png" alt="Doctor Image" />
+                <h1>Hello there!</h1>
+                <h3>Let's get started with RAPPT.</h3>
+        </div>
     )
 }
+
