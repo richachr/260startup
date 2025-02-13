e46ed888a8f92c26b7e06fcf0d23ec6614156dad
richachr <techzgamingdevs@gmail.com>
2025-02-13 13:15:57 -0700

    Converted HTML elements to JSX.

--- startup/src/postAuth/create/create.jsx
+++ startup/src/postAuth/create/create.jsx
@@ -1,22 +1,25 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 export function CreateAppointment() {
+
+    const navigate = useNavigate();
+
     return (
-        <div class="mainContent">
-            <img src="papers.png" alt="Person doing paperwork">
+        <div className="mainContent">
+            <img src="papers.png" alt="Person doing paperwork" />
             <h1>Appointment, you say?</h1>
             <h3>Let's get the forms filled out.</h3>
             <form action="scheduler.html" method="post">
-                <div class="formItem">
+                <div className="formItem">
                     <label for="patientName">What's the full name of the patient?</label>
-                    <input type="text" name="patientName" id="patientName" placeholder="John Doe">
+                    <input type="text" name="patientName" id="patientName" placeholder="John Doe" />
                 </div>
-                <div class="formItem">
+                <div className="formItem">
                     <label for="dob">What's the patient's date of birth?</label>
-                    <input type="date" name="dob" id="dob" placeholder="1/1/1970">
+                    <input type="date" name="dob" id="dob" placeholder="1/1/1970" />
                 </div>
-                <div class="formItem">
+                <div className="formItem">
                     <label for="gender">What's the patient's gender?</label>
                     <select name="gender" id="gender">
                         <option></option>
@@ -24,15 +27,15 @@ export function CreateAppointment() {
                         <option value="m">Male</option>
                     </select>
                 </div>
-                <div class="formItem">
+                <div className="formItem">
                     <label for="phone">What's the best phone number for contacting the patient?</label>
-                    <input type="tel" name="phone" id="phone" placeholder="(801) 555-2039">
+                    <input type="tel" name="phone" id="phone" placeholder="(801) 555-2039" />
                 </div>
-                <div class="formItem">
+                <div className="formItem">
                     <label for="address">What's the patient's address?</label>
-                    <input type="text" name="address" id="address">
+                    <input type="text" name="address" id="address" />
                 </div>
-                <div class="formItem">
+                <div className="formItem">
                     <label for="doctor">Which doctor are you looking to see?</label>
                     <select name="doctor" id="doctor">
                         <option></option>
@@ -41,7 +44,7 @@ export function CreateAppointment() {
                         <option value="3">Dr. Michael Jenkins</option>
                     </select>
                 </div>
-                <div class="formItem">
+                <div className="formItem">
                     <label for="purpose">What's the purpose of the appointment?</label>
                     <select name="purpose" id="purpose">
                         <option></option>
@@ -53,22 +56,22 @@ export function CreateAppointment() {
                         <option value="test">Testing/Diagnosis</option>
                     </select>
                 </div>
-                <div class="formItem">
+                <div className="formItem">
                     <label for="pain">How would you rate your pain on a scale of 1-10?</label>
-                    <input type="number" name="pain" id="pain" min="1" max="10">
+                    <input type="number" name="pain" id="pain" min="1" max="10" />
                 </div>
-                <div class="formItem">
+                <div className="formItem">
                     <label for="urgency">How would you rate the urgency of your appointment on a scale of 1-10?</label>
-                    <input type="number" name="urgency" id="urgency" min="1" max="10">
+                    <input type="number" name="urgency" id="urgency" min="1" max="10" />
                 </div>
-                <div class="formItem">
+                <div className="formItem">
                     <label for="symptoms">What are your symptoms?</label>
-                    <input type="text" name="symptoms" id="symptoms">
+                    <input type="text" name="symptoms" id="symptoms" />
                 </div>
             </form>
             <nav>
-                <a href="appointments.html"><button type="button" class="danger"><span>Back</span></button></a>
-                <a href="scheduler.html"><button type="button" class="primary"><i class="fa-solid fa-arrow-right"></i></button></a>
+                <button onClick={() => navigate(-1)} type="button" className="danger"><span>Back</span></button>
+                <NavLink to="scheduler"><button type="button" className="primary"><i className="fa-solid fa-arrow-right"></i></button></NavLink>
             </nav>
         </div>
     )
