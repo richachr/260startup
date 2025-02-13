8bf400791da1f6704761adb832e2daa6b6c2232b
richachr <techzgamingdevs@gmail.com>
2025-02-12 19:05:08 -0700

    Imported Outlet.

--- startup/src/preAuth/PreAuthHeader.jsx
+++ startup/src/preAuth/PreAuthHeader.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import {NavLink, Outlet} from 'react-router-dom';
 import './welcome/welcome.css';
 
 export default function PreAuthHeader() {
