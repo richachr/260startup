58740afd2302c8e3a19cd2796b8e8375ec47365f
richachr <techzgamingdevs@gmail.com>
2025-02-13 23:11:53 -0700

    Fixed minor bugs, added FA.

--- startup/src/postAuth/PostAuthHeader.jsx
+++ startup/src/postAuth/PostAuthHeader.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import {NavLink, Outlet} from 'react-router-dom';
 import './after_login.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell } from '@fortawesome/free-regular-svg-icons';
+import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 export default function PostAuthHeader() {
     return (
         <div className="reactContent">
-            <header>
+            <header id='postAuth'>
                 <div className="leftContent">
                     <NavLink to="/" className="logo"><img src="RapptLogo.png" alt="Logo" className="logo" /><h2 className="logo">Rappt</h2></NavLink>
                 </div>
                 <div className="rightContent">
                     <nav>
-                        <button className="secondary"><i className="fa-regular fa-bell"></i>
+                        <button className="secondary"><FontAwesomeIcon icon={faBell} className='fontAwesome' />
                             <aside className="notifications">
                                 <h5>Notifications</h5>
                                 <table>
-                                    <tr><td><span>Dr. Smithfield</span> <span>scheduled</span> an appointment for <span>James Haskell</span> on <span>February 25th, 2025</span>.</td></tr>
-                                    <tr><td><span>Dr. Jenkins</span> <span>cancelled</span> an appointment for <span>Emily Haskell</span> on <span>March 14th, 2025</span>.</td></tr>
+                                    <tbody><tr><td><span>Dr. Smithfield</span> <span>scheduled</span> an appointment for <span>James Haskell</span> on <span>February 25th, 2025</span>.</td></tr>
+                                    <tr><td><span>Dr. Jenkins</span> <span>cancelled</span> an appointment for <span>Emily Haskell</span> on <span>March 14th, 2025</span>.</td></tr></tbody>
                                 </table>
                             </aside>
                         </button>
-                        <NavLink to='/'><button className="danger"><i className="fa-solid fa-arrow-right-from-bracket"></i></button></NavLink>
+                        <NavLink to='/'><button className="danger"><FontAwesomeIcon icon={faArrowRightFromBracket} className='fontAwesome' /></button></NavLink>
                     </nav>
                     <h5>Hey there, James!</h5>
                 </div>
