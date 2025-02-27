29814f1403df8672446ab98226397d860287aeed
richachr <techzgamingdevs@gmail.com>
2025-02-26 19:13:41 -0700

    Added logic to fetch and render appointments.

--- startup/src/postAuth/appointments/appointments.jsx
+++ startup/src/postAuth/appointments/appointments.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
@@ -16,6 +16,57 @@ function exportAppointments(userName) {
     tempLink.remove();
 }
 
+function fetchAppointments(userName) {
+    const userData = JSON.parse(localStorage.getItem(userName));
+    return userData.appointments;
+}
+
+function AppointmentsBlock(props) {
+    const [appointments, setAppointments] = React.useState(fetchAppointments(props.userName));
+
+    useEffect(() => {
+        const newAppointments = fetchAppointments(props.userName);
+        if (JSON.stringify(newAppointments) !== JSON.stringify(appointments)) {
+            setAppointments(newAppointments);
+        }
+    },[props.userName,appointments]);
+    return (
+        <div className="appointments">
+            {appointments.map((value) => {
+                return <Appointment data={value} key={Object.keys(value)[0]} />;
+            })}
+        </div>
+    )
+}
+
+function Appointment({data}) {
+    const apptInfo = Object.values(data)[0];
+    return (
+        <div className="appointment" id={Object.keys(data)[0]}>
+            <span>{apptInfo.doctor}</span>
+            <span>{apptInfo.time ? apptInfo.time : "Unscheduled"}</span>
+            <div className="additionalApptInfo">
+                <p>Patient Name: {apptInfo.name}</p>
+                <p>Date of Birth: {apptInfo.dateOfBirth}</p>
+                <p>Gender: {apptInfo.gender}</p>
+                <p>Phone Number: {apptInfo.phone}</p>
+                <p>Address: {apptInfo.address}</p>
+                <p>Purpose: {apptInfo.purpose}</p>
+                <p>Pain (1-10): {apptInfo.painLevel}</p>
+                <p>Urgency (1-10): {apptInfo.urgencyLevel}</p>
+                <p>Symptoms: {apptInfo.symptoms}</p>
+                <p>Inferred Diagnosis: {apptInfo.diagnosis ? apptInfo.diagnosis : "None"}</p>
+                <p>Seriousness: {apptInfo.seriousness ? apptInfo.seriousness : "Uncalculated"}</p>
+                <p>Scheduling Class: {apptInfo.schedulingClass ? apptInfo.schedulingClass : "Uncalculated"}</p>
+            </div>
+            <div className="apptActions">
+                <button className="secondary" onClick="showInfo()"><FontAwesomeIcon icon={faFileLines} className="fontAwesome" /></button>
+                <button className="primary" onClick="editAppt()"><FontAwesomeIcon icon={faPenToSquare} className="fontAwesome" /></button>
+            </div>
+        </div>
+    )
+}
+
 export function Appointments(props) {
     return (
         <div className="mainContent" id="postAuth">
@@ -25,58 +76,7 @@ export function Appointments(props) {
                 <NavLink to="/create-appointment"><button className="primary"><FontAwesomeIcon icon={faCalendarPlus} className="fontAwesome" /><span>New</span></button></NavLink>
                 <button className="secondary" onClick={() => exportAppointments(props.userName)}><FontAwesomeIcon icon={faArrowUpRightFromSquare} className="fontAwesome" /><span>Export</span></button>
             </div>
-            <div className="appointments">
-                <div className="appointment">
-                    <span>Andrew Smithfield</span>
-                    <span>3:00pm, February 5th, 2025</span>
-                    <div className="additionalApptInfo">
-                        <p>Patient Name: James Haskell</p>
-                        <p>Date of Birth: 04/01/2000</p>
-                        <p>Gender: Male</p>
-                        <p>Phone Number: 385-555-3099</p>
-                        <p>Address: 3445 Cougar Rd., Provo, Utah, 84606</p>
-                        <p>Email: segfault@gmail.com</p>
-                        <p>Purpose: Illness</p>
-                        <p>Pain (1-10): 4</p>
-                        <p>Urgency (1-10): 5</p>
-                        <p>Symptoms: Fever, Vomiting</p>
-                        <p>Deception Margin: 0</p>
-                        <p>Inferred Diagnosis: Seasonal Flu</p>
-                        <p>Seriousness: 6</p>
-                        <p>Scheduling Class: 3</p>
-                    </div>
-                    <div className="apptActions">
-                        <button className="secondary" onClick="showInfo()"><FontAwesomeIcon icon={faFileLines} className="fontAwesome" /></button>
-                        <button className="primary" onClick="editAppt()"><FontAwesomeIcon icon={faPenToSquare} className="fontAwesome" /></button>
-                    </div>
-                </div>
-                <div className="appointment">
-                    <span>Sandra Martinez</span>
-                    <span>12:05pm, February 25th, 2025</span>
-                    <div className="additionalApptInfo">
-                        <p>Patient Name: Alexi Haskell</p>
-                        <p>Date of Birth: 08/15/2019</p>
-                        <p>Gender: Female</p>
-                        <p>Phone Number: 385-555-3099</p>
-                        <p>Address: 3445 Cougar Rd., Provo, Utah, 84606</p>
-                        <p>Email: segfault@gmail.com</p>
-                        <p>Purpose: Check-Up</p>
-                        <p>Pain (1-10): 1</p>
-                        <p>Urgency (1-10): 1</p>
-                        <p>Symptoms: None</p>
-                        <div className="hiddenItems">
-                            <p>Deception Margin: 0</p>
-                            <p>Inferred Diagnosis: None</p>
-                            <p>Seriousness: 1</p>
-                            <p>Scheduling Class: 7</p>
-                        </div>
-                    </div>
-                    <div className="apptActions">
-                        <button className="secondary" onClick="showInfo()"><FontAwesomeIcon icon={faFileLines} className="fontAwesome" /></button>
-                        <button className="primary" onClick="editAppt()"><FontAwesomeIcon icon={faPenToSquare} className="fontAwesome" /></button>
-                    </div>
-                </div>
-            </div>
+            <AppointmentsBlock userName={props.userName} />
         </div>
     );
 }
