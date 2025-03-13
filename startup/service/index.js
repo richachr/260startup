1383180a8180b6c8a84c5dcb02dcfb12799ca3c9
richachr <techzgamingdevs@gmail.com>
2025-03-13 09:56:03 -0600

    Added express router and data structures.

--- startup/service/index.js
+++ startup/service/index.js
@@ -10,6 +10,41 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.static('public'));
 
+var apiRouter = express.Router();
+app.use('/api', apiRouter);
+
+let users = {"test@test.test": {
+    "name": "test",
+    "email": "test@test.test",
+    "dateOfBirth": "2025-02-26",
+    "hashedPassword": "$2b$10$fU0RTDHctl2g4cdgeWkfMO.1RK8uOVdvfBRWLlVJYGJiNqXuAtU0y",
+    "appointments": [
+        {
+            "7a925dec-6f53-419b-86b4-fd83a89edb48": {
+                "name": "test",
+                "dateOfBirth": "2025-02-26",
+                "gender": "Male",
+                "phone": "8018018011",
+                "address": "9 Heritage Halls #4104",
+                "doctor": "james.howard@ihc.org",
+                "purpose": "Non-urgent Treatment",
+                "painLevel": "3",
+                "urgencyLevel": "3",
+                "symptoms": "Owie",
+                "diagnosis": "Needs medical care.",
+                "seriousness": 5,
+                "schedulingClass": 4,
+                "time": "Fri Mar 14 2025 11:00:00 GMT-0600 (Mountain Daylight Time)"
+            }
+        }
+    ],
+    "gender": "Male",
+    "phone": "8018018011",
+    "address": "9 Heritage Halls #4104"
+}};
+let doctors = {};
+
+
 
 app.listen(port, () => {
     console.log("Webserver started.")
