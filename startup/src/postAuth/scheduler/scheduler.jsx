b3f8ce2cdc6eef26a5fb780403944ee0e6ab2694
richachr <techzgamingdevs@gmail.com>
2025-03-13 09:45:34 -0600

    Removed uneeded import.

--- startup/src/postAuth/scheduler/scheduler.jsx
+++ startup/src/postAuth/scheduler/scheduler.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
