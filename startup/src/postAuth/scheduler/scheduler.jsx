3ee8693455fc7aa7997fedf380e710e98548cf4e
richachr <techzgamingdevs@gmail.com>
2025-03-15 19:59:33 -0600

    Added calls to get appointment times and update appointment data, moved some TimesAvailable functionality to backend.

--- startup/src/postAuth/scheduler/scheduler.jsx
+++ startup/src/postAuth/scheduler/scheduler.jsx
@@ -1,50 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
-function determineSchedulingClass(apptData) {
-    const pain = +Object.values(apptData)[0].painLevel;
-    const urgency = +Object.values(apptData)[0].urgencyLevel;
-    const seriousness = Object.values(apptData)[0].seriousness;
-    return Math.ceil((pain+urgency+seriousness)/3);
-}
-
-function getChatgptResponse(apptData) {
-    Object.values(apptData)[0].diagnosis = "Needs medical care."
-    Object.values(apptData)[0].seriousness = 5
-    return apptData;
-}
-
-function* timeGenerator(suggestedDate) {
-    suggestedDate.setSeconds(0);
-    while(true) {
-        if(suggestedDate.getHours() > 16) {
-            suggestedDate.setHours(9);
-            suggestedDate.setDate((suggestedDate.getDate()+1));
-        } else if(suggestedDate.getHours() < 9) {
-            suggestedDate.setHours(9);
-        }
-        if(!(1<=suggestedDate.getDay() && suggestedDate.getDay()<=5)) {
-            suggestedDate.setDate((suggestedDate.getDate()+1));
-        }
-        if(1<=suggestedDate.getMinutes() && suggestedDate.getMinutes()<=29) {
-            suggestedDate.setMinutes(30);
-        } else if(31<=suggestedDate.getMinutes() && suggestedDate.getMinutes()<=59) {
-            suggestedDate.setMinutes(60);
-        }
-        yield new Date(suggestedDate);
-        suggestedDate.setMinutes((suggestedDate.getMinutes()+30));
-    }
-}
-
 function TimesAvailable({onTimeSelect,schedulingClass}) {
-    const classDateBindings = {10: 0, 9: 1, 8: 2, 7:3, 6:5, 5:7, 4:14, 3:30, 2:60, 1:180}
-    const currentDay = new Date();
-    const suggestedDate = new Date();
-    suggestedDate.setDate((currentDay.getDate() + classDateBindings[schedulingClass]));
-    const generator = React.useRef(timeGenerator(suggestedDate));
-    const [apptOptions, setApptOptions] = React.useState([generator.current.next().value,generator.current.next().value,generator.current.next().value]);
+    const [offset, setOffset] = React.useState(0);
+    const [apptOptions, setApptOptions] = React.useState(getAppointmentTimes(schedulingClass,offset));
     return (
         <fieldset className="fieldset" onChange={(e) =>onTimeSelect(e.target.value)}>
             {apptOptions.map((value,index) => { return(
@@ -53,27 +14,57 @@ function TimesAvailable({onTimeSelect,schedulingClass}) {
                 </div>
             )})}
             <div className="actions">
-                <button type="button" onClick={() => {setApptOptions([generator.current.next().value,generator.current.next().value,generator.current.next().value])}} className="secondary"><span>Show more times</span></button>
+                <button type="button" onClick={() => {setOffset(offset+3); setApptOptions(schedulingClass,offset)}} className="secondary"><span>Show more times</span></button>
             </div>
         </fieldset>
     )
+}
+
+async function updateApptData(currentApptId,currentApptData) {
+    const response = await fetch('/api/appoointments/schedule/getSchedule',{
+        method: "POST",
+        body: {
+            'appointmentId': currentApptId,
+            'appointmentData': currentApptData
+        },
+        headers: {
+            "Content-type": 'application/json;'
+        }
+    })
+    return response.body.data;
+}
+
+async function getAppointmentTimes(schedulingClass,offset) {
+    const response = await fetch('/api/appointments/schedule/getTimes', {
+        method: "POST",
+        body: {'offset': offset, 'schedulingClass': schedulingClass},
+        headers: {"Content-type": 'application/json;'}
+    })
+    return response.body.times;
 }
 
 // props: userName={userName} currentApptId={currentApptId} currentApptData={currentApptData}
 export function Scheduler(props) {
     const navigate = useNavigate();
     const [selectedTime, setSelectedTime] = React.useState();
-    let apptData = getChatgptResponse(props.currentApptData);
-    const localSchedulingClass = determineSchedulingClass(apptData);
-    Object.values(apptData)[0].schedulingClass = localSchedulingClass;
-    const handleSubmit = (event) => {
-        Object.values(apptData)[0].time = selectedTime
-        let cloudData = JSON.parse(localStorage.getItem(props.userName));
-        let doctorCloudData = JSON.parse(localStorage.getItem(Object.values(apptData)[0].doctor));
-        cloudData.appointments.push(apptData);
-        doctorCloudData.appointments.push(apptData);
-        localStorage.setItem(props.userName,JSON.stringify(cloudData));
-        localStorage.setItem(Object.values(apptData)[0].doctor,JSON.stringify(doctorCloudData));
+    let apptData;
+    useEffect(async () => {
+        apptData = await updateApptData(props.currentApptId,props.currentApptData);
+    },[])
+    localSchedulingClass = apptData.schedulingClass;
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        apptData.time = selectedTime;
+        await fetch('/api/appoointments/schedule/setAppointment',{
+            method: "POST",
+            body: {
+                'appointmentId': currentApptId,
+                'appointmentData': currentApptData
+            },
+            headers: {
+                "Content-type": 'application/json;'
+            }
+        })
         props.onCurrentApptChange(null,null);
         navigate('/appointments');
     }
@@ -82,7 +73,7 @@ export function Scheduler(props) {
             <img id="postAuth" src="papers.png" alt="Person doing paperwork" />
             <h1>Let's talk time.</h1>
             <h3>How do these appointment times sound?</h3>
-            <TimesAvailable onTimeSelect={(time) => setSelectedTime(time)}schedulingClass={localSchedulingClass} />
+            <TimesAvailable onTimeSelect={(time) => setSelectedTime(time)} schedulingClass={localSchedulingClass} />
             <nav>
                 <button onClick={()=>navigate(-1)} type="button" className="danger"><span>Back</span></button>
                 <button onClick={handleSubmit} type="button" className="primary"><FontAwesomeIcon icon={faArrowRight} className="fontAwesome" /></button>
